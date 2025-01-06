d4a47a0bc2a7bffd95450b825ddf5a75a67cbf2c
Felipe Víctor <felipe@oitsys.com>
2025-01-06 17:49:55 -0600

    The aviso-de-privacidad file was added

---
+++ aviso-de-privacidad.php
@@ -0,0 +1,91 @@
+<?php
+	$pageTitle = "Aviso de Privacidad";
+	require_once 'header.php';
+?>
+		<div class="gtco-section">
+			<div class="gtco-container">
+				<main>
+					<!-- BEGIN Fragment Page -->
+					<h1><?php echo $pageTitle ?>.</h1>
+					<section>
+							<h2>Identidad y Domicilio del Responsable.</h2>
+							<p>
+							<strong>SahuayoMich.MX</strong> (en adelante, "el Sitio Web"), con domicilio en Nicolás Bravo #199, Col. La Forestal, Sahuayo Michoacán,
+							es responsable del tratamiento de los datos personales proporcionados por sus usuarios de conformidad con este aviso de privacidad.
+							</p>
+					</section>
+					<section>
+							<h2>Finalidades del Tratamiento de Datos.</h2>
+							<p>Los datos personales que recabamos serán utilizados para las siguientes finalidades necesarias:</p>
+							<ul>
+								<li><strong>Provisión de Servicios:</strong> Para ofrecerle los servicios y funcionalidades disponibles en nuestro sitio web.</li>
+								<li><strong>Comunicación y Atención:</strong> Para responder a sus consultas, solicitudes o proporcionar información relacionada con nuestro contenido o servicios.</li>
+								<li><strong>Mejora de Experiencia:</strong> Para analizar y mejorar la experiencia de navegación en el Sitio Web.</li>
+							</ul>
+					</section>
+					<section>
+							<h2>Datos Personales Recopilados.</h2>
+							<p>Podemos recopilar los siguientes datos personales:</p>
+							<ul>
+								<li>Nombre completo.</li>
+								<li>Dirección de correo electrónico.</li>
+								<li>
+									Información recopilada a través de cookies para mejorar la funcionalidad del sitio Web
+									(consulte nuestra <a href="#">Política de Cookies</a>).
+								</li>
+							</ul>
+					</section>
+					<section>
+							<h2>Transferencias de Datos.</h2>
+							<p>
+							<strong>SahuayoMich.MX</strong> no transfiere sus datos personales a terceros sin su consentimiento,
+							salvo aquellas transferencias necesarias para cumplir con obligaciones legales.
+							</p>
+					</section>
+					<section>
+							<h2>Derechos ARCO.</h2>
+							<p>
+							Usted tiene derecho a acceder, rectificar, cancelar u oponerse (derechos ARCO) al tratamiento de sus datos personales.
+							Para ejercer estos derechos, puede enviar su solicitud a nuestro correo electrónico
+							<a href="mailto:contacto@sahuayomich.mx">contacto@sahuayomich.mx</a> o presentarla en [Dirección Completa].
+							Su solicitud debe contener:
+							</p>
+							<ul>
+								<li>Nombre completo para comunicarle la respuesta.</li>
+								<li>Documentos que acrediten su identidad o representación legal.</li>
+								<li>Descripción clara de los datos personales sobre los que desea ejercer algún derecho.</li>
+								<li>Cualquier información adicional que facilite la localización de los datos.</li>
+							</ul>
+					</section>
+					<section>
+							<h2>Opciones para Limitar el Uso de sus Datos.</h2>
+							<p>
+							Puede limitar el uso de sus datos personales contactándonos al correo <a href="mailto:contacto@sahuayomich.mx">contacto@sahuayomich.mx</a>.
+							Asimismo, puede gestionar sus preferencias relacionadas con cookies en nuestro sitio.
+							</p>
+					</section>
+					<section>
+						<h2>Cambios al Aviso de Privacidad.</h2>
+						<p>
+							Nos reservamos el derecho de realizar cambios o actualizaciones a este aviso de privacidad. Cualquier modificación
+							estará disponible en nuestra página web <a href="https://sahuayomich.mx">https://sahuayomich.mx</a>. Recomendamos
+							revisarlo periódicamente.
+						</p>
+					</section>
+					<section>
+							<h2>Contacto.</h2>
+							<p>Si tiene preguntas sobre este aviso de privacidad, puede contactarnos a través de:</p>
+							<ul>
+								<li><strong>Correo Electrónico:</strong> <a href="mailto:contacto@sahuayomich.mx">contacto@sahuayomich.mx</a></li>
+								<li><strong>Teléfono:</strong> (353) 172-7002</li>
+								<li><strong>Domicilio:</strong> Nicolás Bravo #199, Col. La Forestal, Sahuayo Michoacán.</li>
+							</ul>
+					</section>
+					<p><strong>Fecha de última actualización:</strong> 02/12/2024</p>
+					<!-- END Fragment Page -->
+				</main>
+			</div>
+		</div>
+<?php
+	require_once 'footer.php';
+?>
